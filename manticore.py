a3f717600e06e39df8c4f13d2772c4d54ab1e491
milomccarty99 <milomccarty99@gmail.com>
2023-10-10 13:24:45 -0700

    changes to check in

---
+++ manticore.py
@@ -0,0 +1,9 @@
+trq = input("Please tell me your name: ")
+print("Nice to meet you " + trq + ".")
+
+response = "no"
+while (not ("yes" in  response))
+    
+    response = input("would you like to make a card today?")
+
+print("very good")
